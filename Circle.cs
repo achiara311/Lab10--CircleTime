ddad592440b5cb8cf970018a0d8aa5bc564d4ec9
Anthony Chiara <achiara12@gmail.com>
2019-07-25 18:51:24 -0400

    first commit for Circle Time.

---
+++ Circle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleTime
+{
+    public class Circle
+    {
+        #region fields
+        private double radius;
+
+
+        #endregion
+
+        #region properties
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double _radius, double _formatString)
+        {
+            radius = _radius;
+        }
+        #endregion
+
+        public double FormatCalculateArea()
+        {
+            return Math.Round(CalculateArea(), 2);
+        }
+
+        public double FormatCalculateCircumference()
+        {
+            return Math.Round(CalculateCircumference(), 2);
+        }
+        public double CalculateArea()
+        {   
+            return Math.PI * (radius * radius); 
+        }
+
+        public double CalculateCircumference()
+        {
+            
+            return (2d * radius) * Math.PI;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("The area of the circle is " + FormatCalculateArea());
+            Console.WriteLine("\nThe Circumference of is " + FormatCalculateCircumference());
+        }
+
+
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CircleTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circleNew = new Circle();
+            //double number;
+            bool repeat = true;
+            int iteratons = 0;
+            while (repeat)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number for radius to calculate the area & circumference, mlord/mlady.\n");
+                    string input = Console.ReadLine();
+                    double radius = double.Parse(input);
+                    circleNew.Radius = radius;
+                    circleNew.PrintInfo();
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("\nLiterally not a number. Try again, mlord/mlady.\n");
+                    repeat = true;
+                }
+                Console.WriteLine("\nWould you like to continue?\n");
+                string yesNo = Console.ReadLine();
+                if(yesNo == "n")
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    repeat = true;
+                    
+                }
+               iteratons++;
+            }
+            Console.WriteLine($"\nYou made {iteratons} circles. See you later, nerd.");
+        }
+    }
+}
+
+    
+
+
